d0df0d25f587f69637ce2f6facd746e0603b48ec
root <root@1ac085c32128>
2020-08-11 18:27:18 +0000

    TeaParty

---
+++ src/_07_tea_party/TeaParty.java
@@ -0,0 +1,23 @@
+package _07_tea_party;
+
+public class TeaParty {
+	 public String welcome(String name, boolean isWoman, boolean isKnighted) {
+		 String meet = "Hello ";
+			 
+		 if(isWoman) {
+			 
+			 if(isKnighted) {
+				meet+="Lady ";
+			 }
+			 else {
+				 meet+= "Ms. ";
+			 }
+			 
+		 }
+		 
+		 
+		 
+	        return;
+	        
+	 	}
+}
